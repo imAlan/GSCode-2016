b6044806005de456c8e437d5eef80e21e30b1882
Nazmul Kabir <Nazmul Kabir>
2016-03-05 16:31:21 -0500

    p

--- static/js/main.js
+++ static/js/main.js
@@ -46,9 +46,14 @@ var clean = function(dataset){
     return data;
 };
 
-var generatePair = function(){
-    document.getElementById('pairs').innerHTML = '<div id="pair"> <div class="panel panel-default"> <div class="panel-heading">Pairs Instance</div> <div class="panel-body"> <table class="table"> <tr> <th>Symbol</th> <th>Price</th> <th>Volume</th> <th>MarketCap</th> </tr> <tr> <td id="stock1ticker"></td> <td id="stock1Price"></td> <td id="stock1Vol"></td> <td id="stock1Cap"></td> </tr> <tr> <td id="stock2ticker"></td> <td id="stock2Price"></td> <td id="stock2Vol"></td> <td id="stock2Cap"></td> </tr> <tr> <td id="spread"></td> <td id="DiffPrice"></td> <td id="DiffVol"></td> <td id="DiffCap"></td> </tr> </table> </div> <div id="chart"></div> </div> </div>';
-};
+//var generatePair = function(){
+//    document.getElementById('pairs').innerHTML = '<div id="pair"> <div class="panel panel-default"> <div class="panel-heading">Pairs Instance</div> <div class="panel-body"> ' +
+//        '<table class="table"> ' +
+//        '<tr> <th>Symbol</th> <th>Price</th> <th>Volume</th> <th>MarketCap</th> <th>Ratio</th> </tr> ' +
+//        '<tr> <td id="stock1ticker"></td> <td id="stock1Price"></td> <td id="stock1Vol"></td> <td id="stock1Cap"></td> </tr> ' +
+//        '<tr> <td id="stock2ticker"></td> <td id="stock2Price"></td> <td id="stock2Vol"></td> <td id="stock2Cap"></td> </tr> ' +
+//        '<tr> <td id="spread"></td> <td id="DiffPrice"></td> <td id="DiffVol"></td> <td id="DiffCap"></td> <td id="DiffRatio"></td> </tr> </table> </div> <div id="chart"></div> </div> </div>';
+//};
 
 var ID = 0;
 
@@ -79,7 +84,7 @@ $(document).ready(function(){
                     console.log(datasets);
                 }
                 console.log("display data");
-                generatePair(ID);
+                //generatePair(ID);
                 var data1 = datasets[0]["quote"];
                 var data2 = datasets[1]["quote"];
 
@@ -120,11 +125,11 @@ $(document).ready(function(){
                         console.log(ratio, given_ratio);
 
                         if (ratio <= given_ratio){
-                            //console.log("less than historic");
+                            console.log("less than historic");
                             $("#DiffRatio").addClass("green");
                         }
                         else{
-                            //console.log("greater than historic");
+                            console.log("greater than historic");
                             $("#DiffRatio").removeClass("green");
                         }
                         //Generates the chart each time
